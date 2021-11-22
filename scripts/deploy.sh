5077942ce2822974c0e9c5c638af07005fba20a0
soyeon207 <psy010207@naver.com>
2021-11-23 01:48:05 +0900

    #4 [MODIFY] deploy.sh 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=soyeon-aws-web-service
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/*.jar $REPOSITORY/home/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
@@ -12,7 +12,7 @@ CURRENT_PID=$(pgrep -fl soyeon-aws-web-service | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [-z "$CURRENT_PID"]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -21,6 +21,7 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
+
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
